33aada9632f4a52ae931c874e45098de05cac939
PeteSumners <pete.sumners@utexas.edu>
2018-02-11 21:08:49 -0600

    Done?

--- Project2_starter_files/Project2.cpp
+++ Project2_starter_files/Project2.cpp
@@ -1,8 +1,9 @@
-/* Project2.cpp
- * Peter Sumners
- * EE312, Summer 2017
- * Project 2, Matrix Multiplication
- */
+// matrix-multiplication <Project2.cpp>
+// EE 312 Project 2 submission by
+// Peter Sumners
+// pls945
+// Slip days used: <0>
+// Spring 2018
 
 #include <stdio.h>
 #include <stdint.h>
@@ -16,18 +17,17 @@ void multiplyMatrices(
         double b[],
         const uint32_t b_cols,
         double c[]) {
-    // https://en.wikipedia.org/wiki/Row-_and_column-major_order
 
+    // https://en.wikipedia.org/wiki/Row-_and_column-major_order
     for (int32_t i = 0; i < a_rows; i++) { // Loop through a_rows
         for (int32_t j = 0; j < b_cols; j++) { // Loop through b_cols.
 
-            c[i * a_rows + j] = 0; // Init. result vals. to 0.
+            c[i * b_cols + j] = 0; // Init. result vals. to 0.
             for (int32_t k = 0; k < a_cols; k++) { // Loop through elems (a_cols), and sum.
-                c[i* a_rows + j] += a[i * a_rows + k] * b[k * a_cols + j];
+                c[i * b_cols + j] += a[i * a_cols + k] * b[k * b_cols + j];
             }
         }
     }
-
 }
 
 double** multiplyMatricesPtr(
@@ -67,5 +67,15 @@ double** transposeMatrix(
          double** a,
          const uint32_t a_rows,
          const uint32_t a_cols) {
-    return NULL;
+
+    // Just flip rows and columns.
+    double** result = (double**) malloc(a_cols * sizeof(double*));
+    for (int32_t i = 0; i < a_cols; i++) { // Result rows
+        result[i] = (double*) malloc (a_rows * sizeof(double));
+        for (int32_t j = 0; j < a_rows; j++) {// Result cols
+            result[i][j] = a[j][i];
+        }
+    }
+
+    return result;
 }
